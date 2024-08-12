8985ca9a93f94d9aa0a6af3fe82287a35b4c8430
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-08-12 20:35:26 +0700

    update b11

---
+++ BUOI-11/setup-tailwindcss/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <>
+      <h2>Form dang ky</h2>
+      <button className="bg-[#FFD404] text-black px-3.5 py-[5px] rounded-md">
+        Dang ky ngay
+      </button>
+      <div className="card"></div>
+      <div className="web-39"></div>
+    </>
+  );
+};
+
+export default App;
+
+// Custom CSS taiwindcss: bg-[ma_mau_rgb]
+// Cai khac tuong tu
---
+++ BUOI-11/style-component/src/App.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const App = () => {
+  const Title = styled.h1`
+    color: red;
+  `;
+
+  return <Title>Hello 500 anh em lop web39!</Title>;
+};
+
+export default App;
