84d8c36edd6c5e4f7da94cb87b1d8973c7ddba6b
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-08-21 21:40:23 +0700

    Update pagination

--- PROJECT-ECOMMER/ecom-store/src/components/admin/common/Pagination.jsx
+++ PROJECT-ECOMMER/ecom-store/src/components/admin/common/Pagination.jsx
@@ -1,38 +1,52 @@
 import React from "react";
 
-const PaginationDashboad = () => {
+const PaginationDashboad = ({ totalPages, currentPage, setCurrentPage }) => {
+  // Bien tong trang thanh 1 mang cac so [1,2,3]
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(currentPage);
+
   return (
     <nav aria-label="Page navigation example">
       <ul className="pagination justify-content-center mb-0">
         <li className="page-item">
-          <a className="page-link" href="#" aria-label="Previous">
+          <button
+            className="page-link"
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
             <span aria-hidden="true">Prev</span>
-          </a>
+          </button>
         </li>
-        <li className="page-item active" aria-current="page">
-          <a className="page-link" href="#">
-            1
-          </a>
-        </li>
+
+        {/* Vong lap -> Show phan trang cua minh */}
+        {pageNumbers.map((value) => {
+          return (
+            <li
+              className={`page-item ${value === currentPage ? "active" : ""}`}
+              key={value}
+            >
+              <button
+                className="page-link"
+                onClick={() => setCurrentPage(value)}
+              >
+                {value}
+              </button>
+            </li>
+          );
+        })}
+
         <li className="page-item">
-          <a className="page-link" href="#">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            4
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#" aria-label="Next">
+          <button
+            className="page-link"
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
             <span aria-hidden="true">Next</span>
-          </a>
+          </button>
         </li>
       </ul>
     </nav>
@@ -40,3 +54,26 @@ const PaginationDashboad = () => {
 };
 
 export default PaginationDashboad;
+
+{
+  /* <li className="page-item active" aria-current="page">
+  <a className="page-link" href="#">
+    1
+  </a>
+</li>
+<li className="page-item">
+  <a className="page-link" href="#">
+    2
+  </a>
+</li>
+<li className="page-item">
+  <a className="page-link" href="#">
+    3
+  </a>
+</li>
+<li className="page-item">
+  <a className="page-link" href="#">
+    4
+  </a>
+</li> */
+}
--- PROJECT-ECOMMER/ecom-store/src/hooks/useCallApiNoPagination.jsx
+++ PROJECT-ECOMMER/ecom-store/src/hooks/useCallApiNoPagination.jsx
@@ -1,27 +1,46 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useCallApiNoPagination = (url) => {
+const useCallApiNoPagination = (url, itemsPerPage = 5) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [currentPage, setCurrentPage] = useState(1); //-> State để theo dõi trang hiện tại
+  const [totalPages, setTotalPages] = useState(1); //-> State để lưu trữ tổng số trang
+
+  console.log(currentPage);
+
+
+  let params = `?_page=${currentPage}&_limit=${itemsPerPage}`;
+
+
+  const getApi = async () => {
+    try {
+      // Du lieu dang duoc goi
+      setIsLoading(true);
+      const res = await axios.get(url + params);
+      // Du lieu goi xong roi
+      setData(res.data);
+      setIsLoading(false);
+
+      // Tinh tong so trang Page listing
+      // console.log(res);
+      let totalItems = parseInt(res.headers["x-total-count"]); //Tong SP trong db
+      setTotalPages(Math.ceil(totalItems / itemsPerPage));
+
+      // console.log(totalItems);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
   // Call Api
   useEffect(() => {
-    const getApi = async () => {
-      try {
-        // Du lieu dang duoc goi
-        setIsLoading(true);
-        const res = await axios.get(url);
-        // Du lieu goi xong roi
-        setData(res.data);
-        setIsLoading(false);
-
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getApi();
-  }, [url]);
+  }, [url, currentPage]);
 
 
   // Logic Update Data when remove item -> Cap nhat lai gia tri data
@@ -33,7 +52,7 @@ const useCallApiNoPagination = (url) => {
     return setData(newData);
   }
 
-  return { data, isLoading, removeItem};
+  return { data, isLoading, removeItem, totalPages, currentPage, setCurrentPage };
 }
 
 export default useCallApiNoPagination;
