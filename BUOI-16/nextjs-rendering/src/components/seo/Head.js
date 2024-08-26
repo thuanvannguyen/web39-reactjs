82c64ea541ec711e74df6bb9ef1891079715d981
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2024-08-26 21:12:28 +0700

    update convert project

---
+++ BUOI-16/nextjs-rendering/src/components/seo/Head.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <Head>
+      <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    </Head>
+  )
+}
+
+export default Head
